e57d6a1e649f831aaa45a7ee45b5f04bd78038d6
Hawkman110 <haukurodins@gmail.com>
2016-11-25 13:52:52 +0000

    test

--- HaroldsEndeavour/HaroldsEndeavour/HaroldsEndeavour/HaroldsEndeavour/HaroldsEndeavour.Designer.cs
+++ HaroldsEndeavour/HaroldsEndeavour/HaroldsEndeavour/HaroldsEndeavour/HaroldsEndeavour.Designer.cs
@@ -164,7 +164,6 @@
             this.btnUsePotion.TabIndex = 12;
             this.btnUsePotion.Text = "Use";
             this.btnUsePotion.UseVisualStyleBackColor = true;
-            this.btnUsePotion.Click += new System.EventHandler(this.btnUsePotion_Click);
             // 
             // btnNorth
             // 
